57b3f170dea81ce474f0473246fb91741d9bdeef
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2025-04-16 23:29:39 +0530

    Time: 4 ms (31.43%) | Memory: 8.3 MB (96.16%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i =0;
+        int j=0;
+        string ans = "";
+        while(i<word1.size() || j<word2.size()){
+            if(i<word1.size()) ans.push_back(word1[i]);
+            if(j<word2.size()) ans.push_back(word2[j]);
+            i++;
+            j++;
+        }
+       
+      
+        return ans;
+    }
+};
