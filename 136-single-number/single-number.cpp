56c77c1cb97c67f4a9de2cb526592dcaa53e035a
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2025-07-02 21:43:14 +0530

    Time: 1 ms (27.17%) | Memory: 20.7 MB (26.70%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mpp;
+        // map<int, int> mpp;
 
-        // Count frequency of each number
-        for (int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]]++;
+        // // Count frequency of each number
+        // for (int i = 0; i < nums.size(); i++) {
+        //     mpp[nums[i]]++;
+        // }
+
+        // // Find the number that appears only once
+        // for (auto it : mpp) {
+        //     if (it.second == 1) {
+        //         return it.first;
+        //     }
+        // }
+
+        // return -1; 
+        int res =0;
+        for(int i=0;i<nums.size();i++){
+            res ^= nums[i];
         }
-
-        // Find the number that appears only once
-        for (auto it : mpp) {
-            if (it.second == 1) {
-                return it.first;
-            }
-        }
-
-        return -1; // fallback (not expected if input is valid)
+        return res;
     }
 };
