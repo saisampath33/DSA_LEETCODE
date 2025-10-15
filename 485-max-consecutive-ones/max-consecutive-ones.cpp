52dfd6b799e7c8bdb0c5243a64e73a1e3d6ddbae
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2025-10-16 00:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (34.11%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,10 +6,9 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 cnt++;
-                maxi = max(cnt,maxi);
-            }else{
-                cnt =0;
             }
+            if(nums[i]==0) cnt =0;
+            maxi = max(maxi,cnt);
         }
         return maxi;
     }
